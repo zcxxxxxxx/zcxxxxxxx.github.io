1f7551880db47c4e614fdb9e94547a281b08916a
zcx <2049314799@qq.com>
2023-01-12 23:11:53 +0800

    del some useless file Please enter the commit message for your changes. Lines starting

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/media/zcx/E/zcxxxxxxx.github.io/node_modules/infima/dist/css/default/default.css'),
-  require('/media/zcx/E/zcxxxxxxx.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/media/zcx/E/zcxxxxxxx.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/media/zcx/E/zcxxxxxxx.github.io/src/css/custom.css'),
-];
